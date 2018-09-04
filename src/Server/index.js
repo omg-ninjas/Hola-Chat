3045baaab2550444e45be81e3bf0aaca45677f32
eddierodz7 <eddierodz7@gmail.com>
2018-09-04 18:34:46 -0500

    downloaded deps and socket.io

---
+++ src/Server/index.js
@@ -0,0 +1,12 @@
+var mysql = required('mysql')
+var io = module.exports.io = require('socket.io')(app)
+
+const PORT = process.env.PORT || 3001
+
+const SocketManager = require('./SocketManager')
+
+io.on('connection' , SocketManager)
+
+app.listen(PORT, ()=>{
+  console.log("connected to port:" + PORT);
+})
