68063d63be115347d2cbdc184144d7d7e76cee3b
SthefaniaRodriguezL <stherod13@gmail.com>
2018-09-05 16:03:50 -0500

    Added necessary files

---
+++ src/Component/messaging/Messages.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Messages extends Component {
+
+  constructor() {
+    super();
+  }
+}
+
+   render() {
+     return  (
+       <div></div>
+     );
+   }
