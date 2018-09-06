ca059e717252ed6eae02b4c4eb280fea2c6a5c90
adrianjcorrea <adrianjcorrea007@gmail.com>
2018-09-06 15:23:06 -0500

    added Events component and refactored layout andloginForm, beraly pushing due to wifi problems

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
-  import React, { Component } from 'react';
+import React, { Component } from 'react';
 import io from "socket.io-client";
-import loginForm from './loginForm.js';
+import {USER_CONNECTED, LOGOUT} from '../Events'
+import LoginForm from './LoginForm.js';
 
 const socketUrl = "http://127.0.0.1:3000;"
 export default class Layout extends Component{
@@ -22,11 +23,22 @@ initSocket =() =>{
   })
   this.setState({socket})
 }
+setUser = (user) => {
+  const {socket} = this.state
+   socket.emit(USER_CONNECTED, user);
+   this.setState({user})
+}
+logout = () =>{
+  const [socket] = this.State
+  socket.emit(LOGOUT)
+  this.setState({user:null})
+}
+
 render(){
   const { title } = this.props
   return(
     <div className="container">
-       <loginForm socket={this.props} setUser={this.setUser}/>
+       <LoginForm socket={title} setUser={this.setUser}/>
     </div>
   );
  }
