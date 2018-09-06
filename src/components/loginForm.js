6e6bb4dc5b9780fdee3f62fb54430738eed33371
adrianjcorrea <adrianjcorrea007@gmail.com>
2018-09-06 02:11:49 -0500

    refactored loginForm to confirm able to push

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -9,7 +9,7 @@ export class LoginForm extends Component {
     };
   }
   render(){
-    const { nickname,error } = this.state
+    const { nickname, error } = this.state
     return(
       <div className= "login">
          <form onSubmit={this.handleSubmit} className="login-form">
