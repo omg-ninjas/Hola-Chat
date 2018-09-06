8663b1dbc8d4bf5f6d9ab8d7bd7ede15f3507006
eddierodz7 <eddierodz7@gmail.com>
2018-09-06 15:43:40 -0500

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout.jsx';
+import Layout from './components/Layout'
 import './index.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Layout title='Hola Chat'/>
-      </div>
+        <Layout title="Hola Chat"/>
     );
   }
 }
